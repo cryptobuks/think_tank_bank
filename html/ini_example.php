1d88b7aed1696389ba96144b9e4273dd9a768144
jimmytidey <jimmytidey@gmail.com>
2012-10-30 18:41:26 +0000

    automated commit

--- html/ini_example.php
+++ html/ini_example.php
@@ -4,6 +4,7 @@ define("DB_USER_NAME", 'root');
 define("DB_PASSWORD", 'root');
 define("DB_NAME", 'think_tank_bank');
 define("STAFF_LEFT_AFTER",20);
+define("LOCAL_URL",'http://localhost:88');
 
 include_once __DIR__ .'/includes.php';
 ?>
