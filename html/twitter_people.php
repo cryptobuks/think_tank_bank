0c6760de36b0e19f5ca6c8d9c136256851a057b6
jimmytidey <jimmytidey@gmail.com>
2012-11-11 23:15:35 +0000

    automated commit

--- html/twitter_people.php
+++ html/twitter_people.php
@@ -8,12 +8,22 @@ $people = $db->fetch("SELECT * FROM people WHERE twitter_handle !=''");
 
 foreach($people as $person) {
     echo "<h3>".$person['twitter_handle']."<h3>";
-    $data = file_get_contents("https://api.twitter.com/1/users/show.json?screen_name=".$person['twitter_handle'] );
+    $url = "https://api.twitter.com/1/users/show.json?screen_name=".$person['twitter_handle'];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    
+    // receive server response ...
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $data = curl_exec ($ch);
+    curl_close ($ch);
+    
+    //$data = file_get_contents("https://api.twitter.com/1/users/show.json?screen_name=".$person['twitter_handle'] );
     $data = json_decode($data, true);
     echo "<p>" . $data['followers_count'] . "<p>";
     $date       = time();
     $person_id  = $person['person_id'];
-    $twitter_followers = $data['followers_count'];  
+    $twitter_followers = $data['followers_count'];
     $db->query("INSERT INTO people_twitter_rank (person_id, twitter_followers, date) VALUES ('$person_id', '$twitter_followers', '$date')"); 
 }
 
