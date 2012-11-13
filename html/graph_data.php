de590ad1d3be29d39d4da02f65c54f92d7042b1a
Billy Everyteengi <jimmytidey@gmail.com>
2012-11-13 00:38:36 +0000

    automated commit

--- html/graph_data.php
+++ html/graph_data.php
@@ -19,7 +19,7 @@ foreach ($thinktanks as $thinktank) {
     $i++;
 } 
 
-$sql = 'SELECT *, COUNT(*) as count FROM people_thinktank GROUP BY person_id ORDER BY count DESC LIMIT 100';
+$sql = 'SELECT *, COUNT(*) as count FROM people_thinktank GROUP BY person_id ORDER BY count DESC LIMIT 200';
 
 $jobs = $db->fetch($sql);
 foreach ($jobs as $job) { 
