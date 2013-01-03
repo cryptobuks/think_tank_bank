e86f07439a42edf623c22de52091d935af924a9e
Jimmy Tidey <jimmytidey@gmail.com>
2013-01-03 16:41:20 +0000

    automated commit

--- html/twitter/temp.php
+++ html/twitter/temp.php
@@ -6,9 +6,12 @@ include('../header.php');
 $old_people = $db->fetch("SELECT * FROM people_old WHERE twitter_handle !='' ");
 
 foreach($old_people as $old_person) { 
-    $update_query = "UPDATE people SET twitter_handle='". addslashes($old_person['twitter_handle']) ."' WHERE name_primary= '". addslashes($old_person['name_primary']) ."'";
-    echo "<br/> $update_query <br/>";
-    $db->query($update_query);
+    $update_query = "SELECT * FROM  people WHERE name_primary= '". addslashes($old_person['name_primary']) ."'";
+    
+    $result = $db->fetch($update_query);
+    if (count($result) ==0) { 
+        echo ($old_person['name_primary']);
+    }
 }
 
 
