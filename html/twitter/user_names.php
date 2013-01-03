1cd9d67feedfb396d67ded665d1b40abdea67db8
Jimmy Tidey <jimmytidey@gmail.com>
2013-01-03 18:29:10 +0000

    automated commit

--- html/twitter/user_names.php
+++ html/twitter/user_names.php
@@ -52,6 +52,7 @@ foreach($people as $person) {
    
     echo "<p><strong>" . $thinktank[0]['name'] . "</strong></p>";
     foreach($publications as $publication) {
+        echo "SELECT * FROM publications WHERE publication_id='" . $publication['id'] . "'";
         $titles = $db->fetch("SELECT * FROM publications WHERE publication_id='" . $publication['id'] . "'");
         echo "<p>" . $titles['title'] . "</p>";
     }
