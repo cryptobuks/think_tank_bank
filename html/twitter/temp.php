4a7e407eea17ce40d6fc449a56e2030833a9f176
Jimmy Tidey <jimmytidey@gmail.com>
2013-01-03 17:05:02 +0000

    automated commit

--- html/twitter/temp.php
+++ html/twitter/temp.php
@@ -9,12 +9,12 @@ echo "<ol>";
 foreach($old_people as $old_person) { 
 
     
-    $update_query = "SELECT * FROM  people WHERE name_primary= '". addslashes($old_person['name_primary']) ."' && twitter_handle !=''";
+    $update_query = "SELECT * FROM  people WHERE name_primary= '". addslashes($old_person['name_primary']) ."' && twitter_handle ==''";
     
     $result = $db->fetch($update_query);
     
     if (count($result) == 0) { 
-        echo $update_query . "<br/>" ;
+        echo ."<br/>". $update_query . "<br/>" ;
         echo "<li>" .  $old_person['name_primary']. " -- ". $old_person['twitter_handle'] . "</li>" ;
     }    
     else { 
