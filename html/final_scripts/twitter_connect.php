3c66d31f1f551389d3af82b6c0579016368dca5b
Jimmy Tidey <jimmytidey@gmail.com>
2013-01-15 17:12:21 +0000

    automated commit

--- html/final_scripts/twitter_connect.php
+++ html/final_scripts/twitter_connect.php
@@ -1,6 +1,6 @@
 <?php
 
-set_include_path('/home/96975/domains/think-tanks.jimmytidey.co.uk/final_scripts');
+set_include_path('/home/96975/domains/think-tanks.jimmytidey.co.uk/html/final_scripts');
 require_once('twitteroauth/twitteroauth.php');
 require_once('config.php');
 
