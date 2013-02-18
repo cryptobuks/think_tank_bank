aea7e2478f87de2a45cf2f149919a5657476f6ca
Jimmy Tidey <jimmytidey@gmail.com>
2013-02-18 23:08:27 +0000

    automated commit

--- twitter_scripts/twitter_connect.php
+++ twitter_scripts/twitter_connect.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once( __FILE__ .'/../twitteroauth/twitteroauth.php');
-require_once( __FILE__ .'/../ini.php');
+require_once( __DIR__ .'/../twitteroauth/twitteroauth.php');
+require_once( __DIR__ .'/../ini.php');
 
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_KEY, OAUTH_SECRET);
 ?>
