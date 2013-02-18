0c979803d47f2bd31761868bedd16fcffeb29702
Jimmy Tidey <jimmytidey@gmail.com>
2013-02-18 21:38:28 +0000

    automated commit

--- functions/includes.php
+++ functions/includes.php
@@ -4,18 +4,18 @@ date_default_timezone_set ('Europe/London');
 
 
 //phpQuery;
-require_once ROOT  .'/classes/phpQuery.php';
+require_once __DIR__ .'/../classes/phpQuery.php';
 
 //Instantiate the DB class, constructor will connect to the DB
-require_once  ROOT  .'/classes/dbClass.php';
+require_once  __DIR__ .'/../classes/dbClass.php';
 
 
 //Report and People use classes that extent this class
-require_once ROOT  ."/classes/scraperBaseClass.php";
-require_once ROOT  ."/classes/scraperPeopleClass.php";
-require_once ROOT  ."/classes/scraperPublicationClass.php";
+require_once __DIR__ ."/../classes/scraperBaseClass.php";
+require_once __DIR__ ."/../classes/scraperPeopleClass.php";
+require_once __DIR__ ."/../classes/scraperPublicationClass.php";
 
 
-require_once  ROOT  .'/functions/json.php';
-require_once  ROOT  .'/functions/url_clean.php';
+require_once __DIR__ .'/../functions/json.php';
+require_once __DIR__ .'/../functions/url_clean.php';
 ?>
