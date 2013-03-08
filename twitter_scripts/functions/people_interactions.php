e4452dfdbfffaa6f22a4b96857ac5460a78f2821
Jimmy Tidey <jimmytidey@gmail.com>
2013-03-08 13:52:12 +0000

    automated commit

--- twitter_scripts/functions/people_interactions.php
+++ twitter_scripts/functions/people_interactions.php
@@ -5,7 +5,7 @@ function people_interactions($db, $connection) {
     $cron_monitor = $db->fetch("SELECT * FROM cron_monitor WHERE script='people_interactions'");
     $index = $cron_monitor[0]['index_val'];
 
-    //Update the index for next time
+     //Update the index for next time
     if($index < 800) { 
         
         $db->query("UPDATE cron_monitor SET  index_val = index_val+100 WHERE script = 'people_interactions'");
@@ -16,9 +16,9 @@ function people_interactions($db, $connection) {
     }
     
 
-    
-    $people = $db->fetch("SELECT * FROM people join people_thinktank on people_thinktank.person_id= people.person_id WHERE twitter_id!=''  LIMIT $index, 100" );
-    
+    $query = "SELECT *, count(*) FROM people join people_thinktank on people_thinktank.person_id=people.person_id WHERE twitter_id!=''" ;
+    $people = $db->fetch($query);
+    echo $query;
     print_r($people);    
       
     $return = twitter_interactions($people, $connection, $db,0);
