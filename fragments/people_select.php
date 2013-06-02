df195596473fac8529cff9d5da27d3711fb96b64
Jimmy Tidey <jimmytidey@gmail.com>
2013-06-02 21:15:16 +0100

    automated commit

--- fragments/people_select.php
+++ fragments/people_select.php
@@ -3,19 +3,16 @@
 $old = time() - (60 * 60 * 24 * 2);
 $new = time() - (60 * 60 * 24 * 0);
 
-$people_query = "SELECT *, (tweets.rts - (people.ave_rts *3)) as rate
+$people_query = "SELECT *, ((tweets.rts - (people.ave_rts *3)) - (people.ave_tweets*5) ) as rate
     FROM `tweets`
     JOIN people ON people.twitter_id = tweets.user_id
     WHERE  time > $old && time < $new
     && people.thinktank_name !='' && role!='' && role!='report_author_only' && role!='official twitter acc' && is_rt=0
     ORDER BY rate DESC LIMIT 16";
-
-echo $people_query;
-
+    
 $people = $db->fetch($people_query);
 
 $DEFAULT_ID = $people[0]['person_id'];
-
 
 echo "<ul>";
 foreach($people as $person) {
@@ -24,7 +21,7 @@ foreach($people as $person) {
         <a data-id='" . $person['person_id'] ."' class='person_link'><strong>".$person['name_primary']. "</strong></a>
         (<a class='thinktank_link' data-thinktank-name='".$person['thinktank_name']."'><strong>". $person['thinktank_name'] ."</strong></a>)
         <p>" . $person['text'] . "</p>
-        <p><i class='icon-refresh'></i> " . $person['rts'] . " (".$person['name_primary']. " averages ".$person['ave_rts']. " retweets per tweet)</p>
+        <p><i class='icon-refresh'></i> " . $person['rts'] . " (".$person['name_primary']. " averages ".$person['ave_rts']. " rts tweet & tweets ".$person['ave_tweets']. " times a day ( ".$person['rate']. " ))</p>
          </li>\n";
 }
 echo "</ul>";
--- rt_averages.php
+++ rt_averages.php
@@ -11,8 +11,8 @@
     $edit_query ="SELECT * FROM people 
         JOIN people_thinktank ON people_thinktank.person_id = people.person_id 
         JOIN thinktanks ON thinktanks.thinktank_id = people_thinktank.thinktank_id 
-        
-        LIMIT 1000";
+        WHERE twitter_id !=0
+        LIMIT 10000";
 
     $people = $db->fetch($edit_query);
 
@@ -21,21 +21,25 @@
         
         $twitter_id = $person['twitter_id'];
         $id = $person['person_id']; 
-        
-        $query = "SELECT *, AVG(rts) FROM `tweets`  
-        WHERE user_id='$twitter_id' && is_rt=0
+        $old_time = time()-100 *60*60*24; 
+        $query = "SELECT *, AVG(rts), Count(*) FROM `tweets`  
+        WHERE user_id='$twitter_id' 
+        && is_rt=0
+        && time > $old_time 
         GROUP BY user_id='$twitter_id'
+        
         ";
         
         echo $query . "<br/>";
         
         $tweets = $db->fetch($query);
-        print_r($tweets[0]['AVG(rts)']);
+        
         $ave = $tweets[0]['AVG(rts)'];
+        $ave_tweets = $tweets[0]['Count(*)'] / 100;
         
         //$twitter_image    = addslashes($person['image_url']);
         
-        $query          = "UPDATE people SET ave_rts='$ave' WHERE person_id='$id'";
+        $query          = "UPDATE people SET ave_rts='$ave', ave_tweets='$ave_tweets' WHERE person_id='$id'";
         echo $query . "<br/>";
         $db->query($query);
         
