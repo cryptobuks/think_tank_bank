7a2dc31bff40dcb3ac6c3d76be9b34354fa14d50
Jimmy Tidey <jimmytidey@gmail.com>
2013-06-09 15:12:15 +0100

    automated commit

--- fragments/footer.php
+++ fragments/footer.php
@@ -5,7 +5,7 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="/js/jquery-1.8.0.min.js"><\/script>')</script>
-    
+    <script src="/js/main.js"></script>
     <script src="/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="/js/bootstrap-tab.js"></script>
     
