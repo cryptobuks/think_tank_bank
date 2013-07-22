e71baeb2e75c2e027be6e40ba92f8f980e50d00c
Jimmy Tidey <jimmytidey@gmail.com>
2013-07-22 13:42:31 +0100

    removing curl

--- functions/getTitle.php
+++ functions/getTitle.php
@@ -1,14 +1,7 @@
 <?
 function getUrlTitle($Url){
     
-    $curl = curl_init($Url);
-    curl_setopt($curl, CURLOPT_FAILONERROR, true);
-    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-  
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);  
-    $str = curl_exec($curl);
+    $str = file_get_contents($Url);
     
     if(strlen($str)>0){
         preg_match("/\<title\>(.*)\<\/title\>/",$str,$title);
