cb6c38fd8040ccf0263da9f6ef0626a58f4326ea
Jimmy Tidey <jimmytidey@gmail.com>
2013-07-25 11:46:55 +0100

    google analytics

--- fragments/footer.php
+++ fragments/footer.php
@@ -19,6 +19,15 @@
     
     <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
     <script src="http://cdn.oesmith.co.uk/morris-0.4.3.min.js"></script>
- 
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-42708282-1', 'jimmytidey.co.uk');
+      ga('send', 'pageview');
+
+    </script>
   </body>
 </html>
