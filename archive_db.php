60ccab60698271a13889b5bb66b3949b63cf10ce
Jimmy Tidey <jimmytidey@gmail.com>
2013-10-07 21:49:51 +0100

    automated commit

--- archive_db.php
+++ archive_db.php
@@ -4,7 +4,7 @@ include(__DIR__ . '/ini.php');
 
 $filename = __DIR__ . "/db_backups/backup_".date("m_d_y_B").".sql";
 
-$db_dump_string = "mysqldump -u root -p[".DB_PASSWORD."] [".DB_USER_NAME."] > $filename";
+$db_dump_string = "mysqldump -u [".DB_USER_NAME."] -p[".DB_PASSWORD."] --socket=[".DB_LOCATION."] db96975_think_tank_new   > $filename";
 
 echo $db_dump_string;
 
