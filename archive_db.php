18fc2aed97173a1b6b0c03923a8e069749cf8715
Jimmy Tidey <jimmytidey@gmail.com>
2013-10-07 22:39:24 +0100

    automated commit

--- archive_db.php
+++ archive_db.php
@@ -4,8 +4,15 @@ include(__DIR__ . '/ini.php');
 
 $filename = __DIR__ . "/db_backups/backup_".date("m_d_y_B").".sql";
 
-$db_dump_string = "mysqldump --add-drop-table -h ".DB_LOCATION." -u ".DB_USER_NAME." -p  ".DB_PASSWORD." db96975_think_tank_new  > $filename";
+$db_dump_string = "mysqldump --add-drop-table -h ".DB_LOCATION." -u ".DB_USER_NAME." -p".DB_PASSWORD." db96975_think_tank_new  > $filename";
 
+
+
+
+/* insert DB stuff
+mysql -u db96975_jimmy -pkgG206PV3q82N23 -h internal-db.s96975.gridserver.com db96975_think_tank_old < /nfs/c06/h06/mnt/96975/domains/wonkbook.io/html/db_backups/backup_10_07_13_909.sql
+
+*/
 
 echo $db_dump_string;
 
