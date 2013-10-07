b6db7d745eb2b19cd6d6a1e68676450a663eb095
Jimmy Tidey <jimmytidey@gmail.com>
2013-10-07 22:45:57 +0100

    automated commit

--- archive_db.php
+++ archive_db.php
@@ -6,8 +6,9 @@ $filename = __DIR__ . "/db_backups/backup_".date("m_d_y_B").".sql";
 
 $db_dump_string = "mysqldump --add-drop-table -h ".DB_LOCATION." -u ".DB_USER_NAME." -p".DB_PASSWORD." db96975_think_tank_new  > $filename";
 
-
-
+$str_to_time = strtotime('two weeks ago');
+$sql = "DELETE FROM tweets WHERE time < '$str_to_time'";
+$db->query($sql);
 
 /* insert DB stuff
 mysql -u db96975_jimmy -pkgG206PV3q82N23 -h internal-db.s96975.gridserver.com db96975_think_tank_old < /nfs/c06/h06/mnt/96975/domains/wonkbook.io/html/db_backups/backup_10_07_13_909.sql
